496b13d32f3c04f358f5bf9860b8d8ef95408697
Alan Dao <alan.n.dao@gmail.com>
2017-05-04 14:14:58 -0700

    made vr bridge write quaternions to a memory mapped file

--- CheapVRBridge/Program.cs
+++ CheapVRBridge/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+using System.IO;
 
 namespace CheapVRBridge
 {
@@ -13,40 +11,58 @@ namespace CheapVRBridge
     {
         static void Main(string[] args)
         {
-            bool done = false;
-
-            Console.WriteLine("Hello, World!");
-
             const int listenPort = 11000;
             UdpClient listener = new UdpClient(listenPort);
             IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Waiting for Moonlight-Android in SteamVR mode to start streaming..");
+            //Receive call blocks until we get any udp packet coming in.
+            byte[] blockingStatement = listener.Receive(ref remoteIPEndPoint);
+            Console.WriteLine("Something broadcasted to us in the limited broadcast, assume it's the phone sending head poses.");
+            Console.WriteLine("Beginning while loop of sending quaternion rotation..");
 
-            try
+            bool done = false;
+
+            //create a memory mapped file named "moonlightvr", with size 10000
+            using (MemoryMappedFile mmf = MemoryMappedFile.CreateNew("moonlightvr", 10000))
             {
-                while (!done)
+                //open a view of our memory mapped file as "stream"
+                using (MemoryMappedViewStream stream = mmf.CreateViewStream())
                 {
-                    byte[] bytes = listener.Receive(ref remoteIPEndPoint);
-                    var quaternion = new float[bytes.Length / 4];
-                    for (int i = 0; i < 4; i++)
+
+                    //initialize a writer which will write to our memory mapped file
+                    BinaryWriter writer = new BinaryWriter(stream);
+
+                    //Create a mutex, preventing any process from mutating our view of our quaternion data while we're writing to it.
+                    bool mutexCreated;
+                    Mutex mutex = new Mutex(false, "moonlightvrmutex", out mutexCreated);
+                    while (!done)
                     {
-                        //get the 4 bytes making up the float in big endian form,
-                        //swap the bytes, and then get the float from the byte array
+                        //wait until we can acquire the mutex, the openvr moonlight driver is possibly reading our quaternion data.
+                        mutex.WaitOne();
 
-                        byte[] floatBytes = { bytes[(i * 4) + 0], bytes[(i * 4) + 1], bytes[(i * 4) + 2], bytes[(i * 4) + 3] };
-                        quaternion[i] = ReadSingle(floatBytes, 0, false);
+                        //TODO:: Only get the latest quaternion data. Right now we are possibly looking at old quaternion data sent in a queue.
+                        byte[] bytes = listener.Receive(ref remoteIPEndPoint);
+                        var quaternion = new float[bytes.Length / 4];
+                        for (int i = 0; i < 4; i++)
+                        {
+                            byte[] floatBytes = { bytes[(i * 4) + 0], bytes[(i * 4) + 1], bytes[(i * 4) + 2], bytes[(i * 4) + 3] };
+                            quaternion[i] = ReadSingle(floatBytes, 0, false);
+                        }
+
+                        //set our writer to position 0 in our stream, so we can keep overwriting our old quaternion data whenever we write.
+                        stream.Position = 0;
+                        //write quaternion values to memory mapped file
+                        foreach (float i in quaternion)
+                        {
+                            writer.Write(i);
+                        }
+
+                        //release the mutex, so our openvr moonlight driver has a chance to read quaternion data.
+                        mutex.ReleaseMutex();
                     }
-
-                    //Console.WriteLine("Received broadcast from {0} :\n [{1}]\n", remoteIPEndPoint.ToString(),
-                        //string.Join(", ", quaternion));
+                    writer.Close();
+                    listener.Close();
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            finally
-            {
-                listener.Close();
             }
         }
 
